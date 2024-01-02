527838c37aeb36e3f7e0f11cd547740656c23785
Sunarto <149857174+sunarto-22205067@users.noreply.github.com>
2024-01-02 15:57:12 +0700

    Add files via upload

---
+++ SOAL_1/MahasiswaLama.java
@@ -0,0 +1,29 @@
+package SOAL_1;
+
+/**
+ *
+ * @author mr.sun
+ */
+// MahasiswaLama.java
+public class MahasiswaLama extends Mahasiswa {
+    private int angkatan;
+
+    public MahasiswaLama(String nama, int umur, int angkatan) {
+        super(nama, umur);
+        this.angkatan = angkatan;
+    }
+
+    public int getAngkatan() {
+        return angkatan;
+    }
+
+    public void setAngkatan(int angkatan) {
+        this.angkatan = angkatan;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Angkatan: " + angkatan);
+    }
+}
